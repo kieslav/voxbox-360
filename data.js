840550ad78140e25b1daecea7302e3fe7cde6235
Mariusz Kieslich <mariusz.kieslich@vox.pl>
2024-07-16 15:19:16 +0200

    Initial

---
+++ data.js
@@ -0,0 +1,425 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bathroom",
+      "name": "bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3216566286182445,
+          "pitch": 0.003919261443670052,
+          "rotation": 0,
+          "target": "1-corridor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.800633303585945,
+          "pitch": 0.05849135501966529,
+          "title": "OSŁONKA NA DONICZKĘ OVO BIAŁA Z ROŚLINĄ",
+          "text": "49 zł"
+        }
+      ]
+    },
+    {
+      "id": "1-corridor",
+      "name": "corridor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4890865960879971,
+          "pitch": 0.07946563628109615,
+          "rotation": 0,
+          "target": "0-bathroom"
+        },
+        {
+          "yaw": 0.03901354106634258,
+          "pitch": 0.2885806687407211,
+          "rotation": 0,
+          "target": "2-corridor2"
+        },
+        {
+          "yaw": 0.3391772498904757,
+          "pitch": 0.01493067463810327,
+          "rotation": 0,
+          "target": "3-office"
+        },
+        {
+          "yaw": 1.079844266767882,
+          "pitch": 0.1704557506108486,
+          "rotation": 0,
+          "target": "5-bedroom"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.681274240867566,
+          "pitch": 0.19593136980232373,
+          "title": "SZAFA 2-DRZWIOWA CREATIVE",
+          "text": "&nbsp;1389 zł"
+        },
+        {
+          "yaw": -1.3282723477647167,
+          "pitch": 0.7065457906360972,
+          "title": "ŁAWKA SPOT Z 3 SZUFLADAMI",
+          "text": "1189 zł"
+        },
+        {
+          "yaw": -2.7772590945998417,
+          "pitch": 0.23850431925760418,
+          "title": "DRZWI VOX UPPSALA",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.157723350442099,
+          "pitch": 0.05294718478955218,
+          "title": "LUSTRO WISZĄCE PROSTOKĄTKNE SIMPLE",
+          "text": "599 zł"
+        }
+      ]
+    },
+    {
+      "id": "2-corridor2",
+      "name": "corridor2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.988668757599468,
+          "pitch": 0.3524721477468429,
+          "rotation": 0,
+          "target": "1-corridor"
+        },
+        {
+          "yaw": 2.8558987276510797,
+          "pitch": 0.1479874154878722,
+          "rotation": 0,
+          "target": "3-office"
+        },
+        {
+          "yaw": 0.35370749852368455,
+          "pitch": 0.37805326935687056,
+          "rotation": 0,
+          "target": "4-livingroom"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.201001795307361,
+          "pitch": 0.18541696824193643,
+          "title": "REGAŁ WYSOKI PODWÓJNY CREATIVE",
+          "text": "1789 zł"
+        },
+        {
+          "yaw": -0.08368536062230092,
+          "pitch": 0.4865715548045735,
+          "title": "FOTEL BANCO",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-office",
+      "name": "office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6807345909200375,
+          "pitch": 0.15515285352601893,
+          "rotation": 0,
+          "target": "1-corridor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6450330939088751,
+          "pitch": -0.0918037810791894,
+          "title": "LAMPA PODŁOGOWA HITTA Z ABAŻUREM",
+          "text": "889 zł"
+        },
+        {
+          "yaw": -1.573780636120457,
+          "pitch": 0.24602516059347046,
+          "title": "PANEL TAPICEROWANY GEO SOFORM",
+          "text": "39 zł"
+        },
+        {
+          "yaw": 0.541621707083479,
+          "pitch": 0.4158544592564404,
+          "title": "BIURKO CREATIVE",
+          "text": "1959 zł"
+        },
+        {
+          "yaw": 0.4359027882498321,
+          "pitch": -0.361678646087098,
+          "title": "NADSTAWKA REGAŁU PODWÓJNEGO CREATIVE",
+          "text": "479 zł"
+        },
+        {
+          "yaw": 1.8989380465200503,
+          "pitch": -0.23350687422149718,
+          "title": "SZAFA 3-DRZWIOWA CREATIVE",
+          "text": "2679 zł"
+        },
+        {
+          "yaw": 0.643489663980322,
+          "pitch": 1.3217911577406607,
+          "title": "PANELE PODŁOGOWE VOX QUERRA MAGNA PLUS",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.914736343933768,
+          "pitch": -0.050743036232779204,
+          "title": "PÓŁKA ŚCIENNA YOUNG USERS POJEDYŃĆZA",
+          "text": "328 zł"
+        },
+        {
+          "yaw": -2.923386458057207,
+          "pitch": 0.4766791859466579,
+          "title": "PÓŁKA ŚKOŚNA YOUNG USERS",
+          "text": "309 zł"
+        }
+      ]
+    },
+    {
+      "id": "4-livingroom",
+      "name": "livingroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.370382985103717,
+          "pitch": 0.5013724039746688,
+          "rotation": 0,
+          "target": "2-corridor2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.517322495629789,
+          "pitch": 0.20130554727860428,
+          "title": "PIĘKNY TELEWIZOR",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.591462250608065,
+          "pitch": 0.0521236385985393,
+          "title": "REGAŁ WYSOKI PODWÓJNY CREATIVE",
+          "text": "1789 zł"
+        },
+        {
+          "yaw": -2.9656250569691913,
+          "pitch": 0.10419481788020946,
+          "title": "REGAŁ WYSOKI PODWÓJNY CREATIVE",
+          "text": "2129 zł"
+        },
+        {
+          "yaw": -0.20187722082459558,
+          "pitch": 0.24709741695284926,
+          "title": "STÓŁ ROZKŁADANY CREATIVE<div><br></div>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.876921653457213,
+          "pitch": 1.0276570753250454,
+          "title": "STOLIK KAWOWY PROSTOKĄTNY",
+          "text": "659 zł"
+        },
+        {
+          "yaw": 1.2979073357336617,
+          "pitch": 1.1714524184434616,
+          "title": "PANELE PODŁOGOWE VOX QUERRA MAGNA PLUS DĄB ALVA",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-bedroom",
+      "name": "bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4328906724008537,
+          "pitch": 0.11473519195553195,
+          "rotation": 0,
+          "target": "1-corridor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7250020178247425,
+          "pitch": 0.011438424750735976,
+          "title": "SZAFA 3-DRZWIOWA CREATIVE",
+          "text": "2679 zł"
+        },
+        {
+          "yaw": 1.148662658560319,
+          "pitch": 0.03317252570763074,
+          "title": "LAMPA WISZĄCA Z UCHWYTEM BALL",
+          "text": "429 zł"
+        },
+        {
+          "yaw": 0.4712872792392684,
+          "pitch": 1.2338639546121914,
+          "title": "ŁÓŻKO 160x200 CREATIVE",
+          "text": "2319 zł"
+        },
+        {
+          "yaw": 0.43568768416295534,
+          "pitch": -0.18672965413099618,
+          "title": "PÓŁA WISZĄCA MAŁA 4YOU FRESH POZIOMA",
+          "text": "159&nbsp; zł"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
