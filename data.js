4502db84c7e97a31817ae2ab084747d7b2575412
Mariusz Kieslich <mariusz.kieslich@vox.pl>
2024-07-16 11:28:56 +0200

    Test

---
+++ data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-output",
+      "name": "output",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.6966684071753129,
+          "pitch": 0.4282159474110241,
+          "title": "<div>Komoda Vintage z 3 szufladami</div><div><br></div>",
+          "text": "1329 zł"
+        },
+        {
+          "yaw": -2.0525917070069166,
+          "pitch": 0.5202331423155293,
+          "title": "Fotel Confii",
+          "text": ""
+        },
+        {
+          "yaw": 2.980602269144785,
+          "pitch": 0.22274049351590186,
+          "title": "Szafa 3-drzwiowa Creative",
+          "text": "2609 zł"
+        },
+        {
+          "yaw": 1.6855897838942502,
+          "pitch": 0.49851607518659335,
+          "title": "Łóżeczko Altitude 140x70",
+          "text": "1390 zł"
+        },
+        {
+          "yaw": 0.8134750433328612,
+          "pitch": 0.052387946982250355,
+          "title": "Półka I Young Users pionowa",
+          "text": "608 zł"
+        },
+        {
+          "yaw": 0.18446587771695633,
+          "pitch": 1.0045706384015638,
+          "title": "Panele podłogowe Egger Gome Dąb",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
